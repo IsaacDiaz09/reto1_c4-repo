2c1efc4355227caa9847569b74743cc6871b325c
Isaac Diaz <ivvidp12@gmail.com>
2021-11-20 20:03:12 -0500

    correccion  final end-points rest

--- src/main/java/com/usa/ciclo4/reto1/rest_controller/UserRestController.java
+++ src/main/java/com/usa/ciclo4/reto1/rest_controller/UserRestController.java
@@ -3,11 +3,13 @@ package com.usa.ciclo4.reto1.rest_controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.usa.ciclo4.reto1.model.User;
@@ -23,8 +25,15 @@ public class UserRestController {
 	@Autowired
 	private UserServiceImpl service;
 
-	@PostMapping("/all")
-	private List<User> traerUsuarios(@RequestBody User user) {
+	@GetMapping("/all")
+	private List<User> traerUsuarios() {
+		log.info("Entregando lista de usuarios por peticion rest...");
+		return service.listUsers();
+	}
+
+	@PostMapping("/new")
+	@ResponseStatus(HttpStatus.CREATED)
+	private void guardarUsuario(@RequestBody User user) {
 		try {
 			log.info("Guardando un nuevo usuario en la DB");
 			user.setConfirmPassword(user.getPassword());
@@ -32,8 +41,6 @@ public class UserRestController {
 		} catch (Exception ex) {
 			log.error(ex.getMessage());
 		}
-		log.info("Entregando lista de usuarios por peticion rest...");
-		return service.listUsers();
 	}
 
 	@GetMapping("/{email}")
